4e8d45419e00eae2769c19afadbaae95754c8cb2
Barent Langwell <barent@podiumeducation.com>
2024-06-17 12:48:53 -0700

    Update popper logic and styles

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const popoverTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="popover"]'
-);
+const filterButton = document.getElementById("filter-button");
+const floatingSelect = document.getElementById("floating-select");
 
-const popoverList = [...popoverTriggerList].map(
-  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-);
+const popperInstance = Popper.createPopper(filterButton, floatingSelect, {
+  placement: "bottom",
+  modifiers: [
+    {
+      name: "offset",
+      options: {
+        offset: [0, 8],
+      },
+    },
+  ],
+});
+
+function showPopper() {
+  floatingSelect.setAttribute("data-show", "");
+  popperInstance.update();
+}
+
+function hidePopper() {
+  floatingSelect.removeAttribute("data-show");
+}
+
+const showEvents = ["focus"];
+const hideEvents = ["blur"];
+
+showEvents.forEach((event) => {
+  filterButton.addEventListener(event, showPopper);
+});
+
+hideEvents.forEach((event) => {
+  filterButton.addEventListener(event, hidePopper);
+});
