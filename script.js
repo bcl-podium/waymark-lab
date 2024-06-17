1c038eb49b301bda0f413854086cbe102960d74e
Barent Langwell <barent@podiumeducation.com>
2024-06-17 13:41:31 -0700

    Update popper option styles based on format orientation

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ showEvents.forEach((event) => {
 hideEvents.forEach((event) => {
   filterButton.addEventListener(event, hidePopper);
 });
+
+function updateSelection() {
+  let floatingSelection = document.getElementById("floating-selection");
+  floatingSelection.innerText = "30";
+}
+
+updateSelection();
+
+const floatingOptions = document.querySelectorAll(".floating-select__option");
+floatingOptions.forEach((option) => {
+  if (option.dataset.orientation === "landscape") {
+    console.log(option.firstElementChild.style);
+    option.style.cssText = "width: 40px;";
+    option.firstElementChild.style.cssText = "";
+  } else {
+    option.style.cssText = "width: 25px; height: 35px;";
+  }
+
+  option.addEventListener("click", () => {
+    console.log(option.dataset.duration);
+  });
+});
